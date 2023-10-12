af9c2f5d79cfd036732ca653e000a6c6156b66be
eun1337 <dbdmstj0327@naver.com>
2023-10-12 16:06:13 +0900

    03

--- main.c
+++ main.c
@@ -3,17 +3,40 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void func1(void)
+int sumTwo(int a, int b)
 {
-    int x;
-    printf("func1 x is at %p\n", &x);
+    return a+b;
 }
 
-int main(void)
+int square(int n)
 {
-    int x;
-    printf("main x is at %p\n", &x);
-    func1();
+    return n*n;
+}
 
+int get_max(int x, int y)
+{
+    if (x > y) return x;
+    else return y;
+}
+
+int main()
+{
+    int i, j;
+    
+    printf("정수 두개를 입력하시오. : ");
+	scanf("%d %d", &i, &j);
+    int my_sum=sumTwo(i, j);
+    printf("두 정수의 합 : %d\n", my_sum);
+
+    printf("정수 한개를 입력하시오. : ");
+	scanf("%d", &i);
+    int my_square=square(i);
+    printf("정수의 제곱 : %d\n", my_square);
+    
+    printf("정수 두개를 입력하시오. : ");
+	scanf("%d %d", &i, &j);
+    int my_max=get_max(i, j);
+    printf("큰 값 : %d\n", my_max);
+    
     return 0;
 }
