584d3c3b48315154bed3d270e051ebef4c660f93
eun1337 <dbdmstj0327@naver.com>
2023-10-12 16:27:56 +0900

    04

--- main.c
+++ main.c
@@ -2,41 +2,27 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int sumTwo(int a, int b)
+/*
+void square( int a )
 {
-    return a+b;
-}
-
-int square(int n)
-{
-    return n*n;
-}
-
-int get_max(int x, int y)
-{
-    if (x > y) return x;
-    else return y;
+	a = a*a;
 }
 
 int main()
 {
-    int i, j;
-    
-    printf("정수 두개를 입력하시오. : ");
-	scanf("%d %d", &i, &j);
-    int my_sum=sumTwo(i, j);
-    printf("두 정수의 합 : %d\n", my_sum);
+	int a = 2;
+	square(a);
+	printf("a=%i\n", a);
+}
+*/
+int square( int a )
+{
+	return (a*a);
+}
 
-    printf("정수 한개를 입력하시오. : ");
-	scanf("%d", &i);
-    int my_square=square(i);
-    printf("정수의 제곱 : %d\n", my_square);
-    
-    printf("정수 두개를 입력하시오. : ");
-	scanf("%d %d", &i, &j);
-    int my_max=get_max(i, j);
-    printf("큰 값 : %d\n", my_max);
-    
-    return 0;
+int main()
+{
+	int a = 2;
+	a = square(a);
+	printf("a=%i\n", a);
 }
